6a6edd6195ced272d4d0115c215c2d926fbceabf
RobertoSM19 <jsantana3@ucol.mx>
2025-01-31 08:44:48 -0600

    3.0

---
+++ src/frontend/jquery.js
@@ -0,0 +1,4 @@
+// TODO: Crear los siguientes fronts para consumir la api
+
+// Características del front:
+// * El resultado debe aparecer en un label que tenga la siguiente estructura: Hola, Soy (nombre) y tengo (edad) de edad
---
+++ src/frontend/react.jsx
@@ -0,0 +1,9 @@
+// TODO: Crear los siguientes fronts para consumir la api
+//bootstrap
+
+// Características del front:
+// * Un input por cada campo que se tiene que mandar
+// * Validar en front cada campo obligatorio
+// * Poner un datepicker en fecha
+// * El resultado debe aparecer en un label que tenga la siguiente estructura: Hola, Soy (nombre) y tengo (edad) de edad
+
