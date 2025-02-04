275e3af86f0fdc929cf6bfa212af3f0ba2c3883b
RobertoSM19 <jsantana3@ucol.mx>
2025-02-04 12:19:07 -0600

    Get method added and connection with DB

--- src/functions/postName.js
+++ src/functions/postName.js
@@ -1,51 +1,75 @@
+// TODO: Hacer una lectura de las bases de datos y agregar los datos cuando se manden por el frontend con sequalize
+
 const { app } = require('@azure/functions');
 const validator = require("validator");
 const errors = require("./errorHandler.json");
+const Person = require("../back/db");
 
 app.http('person', {
-    methods: ['POST'],
+    methods: ['POST', 'GET'],
     authLevel: 'anonymous',
     handler: async (req) => {
 
-        let bodyRequest;
+        if(req.method=='GET'){
+            try {
+                const allPeople=await Person.findAll();
 
-        try {
-            bodyRequest = await req.json();
-        } catch (error) {
-            return {
-                status: errors.error00.status,
-                body: JSON.stringify(errors.error00.body),
-                headers: errors.error00.headers
-            };
-        }
-
-        if (!bodyRequest.name) {
-            return { status: errors.error01.status, body: JSON.stringify(errors.error01.body), headers: errors.error01.headers }
-        }
-        if (!bodyRequest.firstSurname) {
-            return { status: errors.error02.status, body: JSON.stringify(errors.error02.body), headers: errors.error02.headers }
-        }
-        if (!bodyRequest.secondSurname) {
-            return { status: errors.error03.status, body: JSON.stringify(errors.error03.body), headers: errors.error03.headers }
-        }
-        if (!bodyRequest.area) {
-            return {
-                status: 400, body: JSON.stringify({
-                    "msg": "Invalid birthdate ",
-                    "errorName": "ERR05"
-                }), headers: { "Content-Type": "application/json" }
+                return {status:200,body:JSON.stringify(allPeople,null,2)};
+            } catch (error) {
+                return {status:500,msg:error};
             }
         }
 
 
+        if (req.method == 'POST') {
+            let bodyRequest;
 
-        try {
-            const age = birthdayValidation(bodyRequest.birthdate);
-            let fullName = !bodyRequest.secondName ? bodyRequest.name.charAt(0).toUpperCase() + bodyRequest.name.slice(1).toLowerCase() : `${bodyRequest.name.charAt(0).toUpperCase() + bodyRequest.name.slice(1).toLowerCase()} ${bodyRequest.secondName.charAt(0).toUpperCase() + bodyRequest.secondName.slice(1).toLowerCase()}`;
-            fullName += " " + bodyRequest.firstSurname.charAt(0).toUpperCase() + bodyRequest.firstSurname.slice(1).toLowerCase() + " " + bodyRequest.secondSurname.charAt(0).toUpperCase() + bodyRequest.secondSurname.slice(1).toLowerCase();
-            return { status: 200, body: JSON.stringify({ fullName: fullName, age: age }), headers: { 'Content-Type': 'application/json' } }
-        } catch (err) {
-            return { status: err.status, body: JSON.stringify(err.body), headers: err.headers }
+            try {
+                bodyRequest = await req.json();
+            } catch (error) {
+                return {
+                    status: errors.error00.status,
+                    body: JSON.stringify(errors.error00.body),
+                    headers: errors.error00.headers
+                };
+            }
+
+            if (!bodyRequest.name) {
+                return { status: errors.error01.status, body: JSON.stringify(errors.error01.body), headers: errors.error01.headers }
+            }
+            if (!bodyRequest.firstSurname) {
+                return { status: errors.error02.status, body: JSON.stringify(errors.error02.body), headers: errors.error02.headers }
+            }
+            if (!bodyRequest.secondSurname) {
+                return { status: errors.error03.status, body: JSON.stringify(errors.error03.body), headers: errors.error03.headers }
+            }
+            if (!bodyRequest.area) {
+                return {
+                    status: 400, body: JSON.stringify({
+                        "msg": "Invalid birthdate ",
+                        "errorName": "ERR05"
+                    }), headers: { "Content-Type": "application/json" }
+                }
+            }
+
+
+            try {
+                const age = birthdayValidation(bodyRequest.birthdate);
+
+                let name = bodyRequest.name.charAt(0).toUpperCase() + bodyRequest.name.slice(1).toLowerCase();
+                let secondName = bodyRequest.secondName.charAt(0).toUpperCase() + bodyRequest.secondName.slice(1).toLowerCase();
+                let firstSurname = bodyRequest.firstSurname.charAt(0).toUpperCase() + bodyRequest.firstSurname.slice(1).toLowerCase();
+                let secondSurname = bodyRequest.secondSurname.charAt(0).toUpperCase() + bodyRequest.secondSurname.slice(1).toLowerCase();
+
+                let fullName = !bodyRequest.secondName ? name : `${name} ${secondName}`;
+                fullName += " " + firstSurname + " " + secondSurname;
+
+                await Person.create({ name: name, secondName: secondName, firstSurname: firstSurname, secondSurname: secondSurname, birthdate: bodyRequest.birthdate, area: bodyRequest.area });
+
+                return { status: 200, body: JSON.stringify({ fullName: fullName, age: age }), headers: { 'Content-Type': 'application/json' } }
+            } catch (err) {
+                return { status: err.status, body: JSON.stringify(err.body), headers: err.headers }
+            }
         }
     }
 });
@@ -61,7 +85,7 @@ function birthdayValidation(birthdate) {
 
     let age = today.getFullYear() - birthdate.slice(0, 4);
 
-    if (Number(birthdate.slice(8, 10)) > Number(today.getDate()) || Number(birthdate.slice(5, 7)) > Number(today.getMonth() + 1)) age--;
+    if (Number(birthdate.slice(8, 10)) > Number(today.getDate()) && Number(birthdate.slice(5, 7)) >= Number(today.getMonth() + 1)) age--;
 
     return age;
 }
