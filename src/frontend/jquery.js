74a5fb64e79fc351581ba17c208357875c621108
RobertoSM19 <jsantana3@ucol.mx>
2025-02-06 20:49:29 -0600

    Archivos beta frontend eliminados

--- src/frontend/jquery.js
+++
@@ -1,4 +0,0 @@
-// TODO: Crear los siguientes fronts para consumir la api
-
-// Características del front:
-// * El resultado debe aparecer en un label que tenga la siguiente estructura: Hola, Soy (nombre) y tengo (edad) de edad
